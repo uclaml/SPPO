cc366a632ccf988f7d16048a481bd8d51a35b633
San <99511815+sanowl@users.noreply.github.com>
2024-07-06 14:24:38 +0300

    .

--- sppo/run_dpo.py
+++ sppo/run_dpo.py
@@ -5,6 +5,7 @@
 import logging
 import random
 import sys
+import yaml
 
 import torch
 import transformers
@@ -27,75 +28,31 @@ from alignment import (
 from peft import PeftConfig, PeftModel
 from trainer import DPOTrainer
 
-
 logger = logging.getLogger(__name__)
 
+def load_config(config_path):
+    with open(config_path, 'r') as config_file:
+        return yaml.safe_load(config_file)
 
-def main():
-    parser = H4ArgumentParser((ModelArguments, DataArguments, DPOConfig))
-    model_args, data_args, training_args = parser.parse()
-    training_args.do_eval = False
-    num_iteration = 1
-    for i in range(num_iteration):
-        # if i > 0:
-        #     model_args.model_name_or_path = f"data/zephry-7b-dpo-qlora-idpo-{i}"
-        # training_args.output_dir = f"data/zephry-7b-dpo-qlora-idpo-{i+1}"
-        main_inner(model_args, data_args, training_args)
-        print(f"-------------------------Finished Iteration {i+1}---------------------------------")
-
-
-def main_inner(model_args, data_args, training_args):
-    #######
-    # Setup
-    #######
+def setup_logging(log_level):
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
-    log_level = training_args.get_process_log_level()
     logger.setLevel(log_level)
     transformers.utils.logging.set_verbosity(log_level)
     transformers.utils.logging.enable_default_handler()
     transformers.utils.logging.enable_explicit_format()
 
-    # Log on each process the small summary:
-    logger.info(f"Model parameters {model_args}")
-    logger.info(f"Data parameters {data_args}")
-    logger.info(f"Training/evaluation parameters {training_args}")
-
-    # Check for last checkpoint
-    last_checkpoint = get_checkpoint(training_args)
-    if last_checkpoint is not None and training_args.resume_from_checkpoint is None:
-        logger.info(f"Checkpoint detected, resuming training at {last_checkpoint=}.")
-
-    # Set seed for reproducibility
-    set_seed(training_args.seed)
-
-    ###############
-    # Load datasets
-    ###############
+def load_and_process_datasets(data_args, tokenizer):
     raw_datasets = get_datasets(data_args, splits=["train"])
     logger.info(
         f"Training on the following splits: {[split + ' : ' + str(dset.num_rows) for split, dset in raw_datasets.items()]}"
     )
     column_names = list(raw_datasets["train"].features)
     column_names = [x for x in column_names if x not in ['chosen_probs', 'chosen_probs_win', 'chosen_probs_lose']]
-    # raw_datasets["train"].add_column("chosen_prob", [1]*len(raw_datasets["train"]))
-    # print(column_names)
-    # exit()
-    # print(column_names)
-    # exit()
 
-    #####################################
-    # Load tokenizer and process datasets
-    #####################################
-    data_args.truncation_side = "left"  # Truncate from left to ensure we don't lose labels in final turn
-    tokenizer = get_tokenizer(model_args, data_args)
-
-    #####################
-    # Apply chat template
-    #####################
     raw_datasets = raw_datasets.map(
         apply_chat_template,
         fn_kwargs={"tokenizer": tokenizer, "task": "dpo", "skip_system_message": True},
@@ -104,24 +61,14 @@ def main_inner(model_args, data_args, training_args):
         desc="Formatting comparisons with prompt template",
     )
 
-    #################################
-    # SPPO: Probability of Preference
-    #################################
-    #raw_datasets["train"] = raw_datasets["train"].add_column(name="chosen_probs", column=[1]*len(raw_datasets["train"]))
-
-    # Replace column names with what TRL needs, text_chosen -> chosen and text_rejected -> rejected
     for split in ["train"]:
         raw_datasets[split] = raw_datasets[split].rename_columns(
             {"text_prompt": "prompt", "text_chosen": "chosen", "text_rejected": "rejected"}
         )
 
-    print(raw_datasets['train'].features)
-    # Log a few random samples from the training set:
-    for index in random.sample(range(len(raw_datasets["train"])), 3):
-        logger.info(f"Prompt sample {index} of the raw training set:\n\n{raw_datasets['train'][index]['prompt']}")
-        logger.info(f"Chosen sample {index} of the raw training set:\n\n{raw_datasets['train'][index]['chosen']}")
-        logger.info(f"Rejected sample {index} of the raw training set:\n\n{raw_datasets['train'][index]['rejected']}")
+    return raw_datasets
 
+def setup_model(model_args, training_args):
     torch_dtype = (
         model_args.torch_dtype if model_args.torch_dtype in ["auto", None] else getattr(torch, model_args.torch_dtype)
     )
@@ -138,7 +85,7 @@ def main_inner(model_args, data_args, training_args):
     )
 
     model = model_args.model_name_or_path
-    if is_adapter_model(model, model_args.model_revision) is True:
+    if is_adapter_model(model, model_args.model_revision):
         logger.info(f"Loading SFT adapter for {model_args.model_name_or_path=}")
         peft_config = PeftConfig.from_pretrained(model_args.model_name_or_path, revision=model_args.model_revision)
         model_kwargs = dict(
@@ -153,7 +100,7 @@ def main_inner(model_args, data_args, training_args):
         base_model = AutoModelForCausalLM.from_pretrained(
             peft_config.base_model_name_or_path,
             **model_kwargs,
-        )
+        ) 
         model = PeftModel.from_pretrained(
             base_model,
             model_args.model_name_or_path,
@@ -164,37 +111,14 @@ def main_inner(model_args, data_args, training_args):
     ref_model = model
     ref_model_kwargs = model_kwargs
 
-    if model_args.use_peft is True:
+    if model_args.use_peft:
         ref_model = None
         ref_model_kwargs = None
 
-    #########################
-    # Instantiate DPO trainer
-    #########################
-    trainer = DPOTrainer(
-        model,
-        ref_model,
-        model_init_kwargs=model_kwargs,
-        ref_model_init_kwargs=ref_model_kwargs,
-        args=training_args,
-        beta=training_args.beta,
-        train_dataset=raw_datasets["train"],
-        # eval_dataset=raw_datasets["test"],
-        tokenizer=tokenizer,
-        max_length=training_args.max_length,
-        max_prompt_length=training_args.max_prompt_length,
-        peft_config=get_peft_config(model_args),
-        loss_type=training_args.loss_type,
-    )
+    return model, ref_model, model_kwargs, ref_model_kwargs
 
-    ###############
-    # Training loop
-    ###############
+def train_and_evaluate(trainer, raw_datasets, training_args):
     checkpoint = None
-    # if training_args.resume_from_checkpoint is not None:
-    #     checkpoint = training_args.resume_from_checkpoint
-    # elif last_checkpoint is not None:
-    #     checkpoint = last_checkpoint
     train_result = trainer.train(resume_from_checkpoint=checkpoint)
     metrics = train_result.metrics
     metrics["train_samples"] = len(raw_datasets["train"])
@@ -204,9 +128,6 @@ def main_inner(model_args, data_args, training_args):
 
     logger.info("*** Training complete ***")
 
-    ##########
-    # Evaluate
-    ##########
     if training_args.do_eval:
         logger.info("*** Evaluate ***")
         metrics = trainer.evaluate()
@@ -214,14 +135,11 @@ def main_inner(model_args, data_args, training_args):
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
 
-    ##################################
-    # Save model and create model card
-    ##################################
+def save_model_and_results(trainer, training_args, model_args, data_args):
     logger.info("*** Save model ***")
     trainer.save_model(training_args.output_dir)
     logger.info(f"Model saved to {training_args.output_dir}")
 
-    # Save everything else on main process
     kwargs = {
         "finetuned_from": model_args.model_name_or_path,
         "dataset": list(data_args.dataset_mixer.keys()),
@@ -230,20 +148,66 @@ def main_inner(model_args, data_args, training_args):
     }
     if trainer.accelerator.is_main_process:
         trainer.create_model_card(**kwargs)
-        # Restore k,v cache for fast inference
         trainer.model.config.use_cache = True
         trainer.model.config.save_pretrained(training_args.output_dir)
 
-    # else:
-    #     exit()
-
-    if training_args.push_to_hub is True:
+    if training_args.push_to_hub:
         logger.info("Pushing to hub...")
         trainer.push_to_hub(**kwargs)
 
     trainer.accelerator.wait_for_everyone()
     logger.info("*** Training complete! ***")
 
+def main():
+    parser = H4ArgumentParser((ModelArguments, DataArguments, DPOConfig))
+    model_args, data_args, training_args = parser.parse()
+    training_args.do_eval = False
+    num_iteration = 1
+
+    try:
+        for i in range(num_iteration):
+            main_inner(model_args, data_args, training_args)
+            print(f"-------------------------Finished Iteration {i+1}---------------------------------")
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        raise
+
+def main_inner(model_args, data_args, training_args):
+    setup_logging(training_args.get_process_log_level())
+
+    logger.info(f"Model parameters {model_args}")
+    logger.info(f"Data parameters {data_args}")
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    last_checkpoint = get_checkpoint(training_args)
+    if last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+        logger.info(f"Checkpoint detected, resuming training at {last_checkpoint=}.")
+
+    set_seed(training_args.seed)
+
+    data_args.truncation_side = "left"
+    tokenizer = get_tokenizer(model_args, data_args)
+    raw_datasets = load_and_process_datasets(data_args, tokenizer)
+
+    model, ref_model, model_kwargs, ref_model_kwargs = setup_model(model_args, training_args)
+
+    trainer = DPOTrainer(
+        model,
+        ref_model,
+        model_init_kwargs=model_kwargs,
+        ref_model_init_kwargs=ref_model_kwargs,
+        args=training_args,
+        beta=training_args.beta,
+        train_dataset=raw_datasets["train"],
+        tokenizer=tokenizer,
+        max_length=training_args.max_length,
+        max_prompt_length=training_args.max_prompt_length,
+        peft_config=get_peft_config(model_args),
+        loss_type=training_args.loss_type,
+    )
+
+    train_and_evaluate(trainer, raw_datasets, training_args)
+    save_model_and_results(trainer, training_args, model_args, data_args)
 
 if __name__ == "__main__":
     main()
